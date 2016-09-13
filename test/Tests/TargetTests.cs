11b77f405c0af64fdf34e7f59520ef915640c048
Jamie Cansdale <jcansdale@gmail.com>
2016-09-13 13:41:59 +0100

    Add source and tests to target .NET and .NET Core.

---
+++ test/Tests/TargetTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Tests
+{
+    using NUnit.Framework;
+    using Targets;
+
+    public class TargetTests
+    {
+        [Test]
+        public void Foo()
+        {
+            Assert.That(Target.Foo(), Is.EqualTo("Bar"));
+        }
+    }
+}
